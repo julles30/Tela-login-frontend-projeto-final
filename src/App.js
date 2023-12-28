d3b82417aaf8c7a2b90c533455c20ed59fdea552
Luísa <112406093+moonlight93-svg@users.noreply.github.com>
2023-12-28 20:07:43 +0000

    Adicionado os links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import LoginSignUp from './components/LoginSignUp/LoginSignUp'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage'
+import LoginSignUp from './components/LoginSignUp/LoginSignUp';
 
 // Este arquivo App.js exporta um componente funcional sem nome (um componente anônimo) que retorna a estrutura da aplicação. 
 // Ele importa um componente chamado LoginSignUp do diretório ./components/LoginSignUp/LoginSignUp e o renderiza dentro de uma <div>.
 
 export default (props) => {
   return (
-    <div>
-      <LoginSignUp />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginSignUp />} />
+      </Routes>
+    </Router>
   )
 };
